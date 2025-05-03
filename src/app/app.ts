18fcfb4e8123083e216c05cae79bf859068bc187
Mohammed Khallaf <mohammedkhallaf.dev@gmail.com>
2025-05-03 15:49:16 +0300

    refactor(app): remove unnecessary server status route

--- src/app/app.ts
+++ src/app/app.ts
@@ -17,9 +17,7 @@ app.use((req, res, next) => {
 app.use(express.json());
 app.use(middleware);
 app.use("/api/v1", routes);
-app.get("*", (req, res) => {
-  res.send("Server is running");
-});
+
 app.use(notFoundHandler);
 app.use(errorHandler);
 
